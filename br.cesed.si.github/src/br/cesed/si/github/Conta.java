7f108fcc834fc6f814bae8ad612b5c6259dceab7
Aluno <aluno@cesed.local>
2017-03-20 11:15:32 -0300

    Enviando 1a ver do proj

---
+++ br.cesed.si.github/src/br/cesed/si/github/Conta.java
@@ -0,0 +1,23 @@
+package br.cesed.si.github;
+
+public class Conta {
+	
+	private int agencia;
+	private int conta;
+	private Pessoa pessoa;
+	public Conta(int agencia, int conta, Pessoa pessoa) {
+		super();
+		this.agencia = agencia;
+		this.conta = conta;
+		this.pessoa = pessoa;
+	}
+	
+		
+	}
+	
+
+	
+		
+	
+	
+
---
+++ br.cesed.si.github/src/br/cesed/si/github/Pessoa.java
@@ -0,0 +1,23 @@
+package br.cesed.si.github;
+
+public class Pessoa {
+	
+	private String nome;
+	private int cpf;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+
+}
